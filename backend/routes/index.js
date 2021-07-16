702818b48ca47888c0931dde5eb4b7fe5b6b6f53
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-07-16 12:57:45 -0400

    Part1:COMPLETED

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,10 +4,9 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
 });
+
 
 module.exports = router;
