927d93e4cd030dc14cf8813044b8203616e0ada9
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-07-21 16:34:01 -0400

    HomePage1.0:Fixed

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,25 +1,25 @@
-//// POST /businesses/:business_id/reviews
-const express = require('express')
-const router = express.Router();
+// //// POST /businesses/:business_id/reviews
+// const express = require('express')
+// const router = express.Router();
 
-const asyncHandler = require('express-async-handler');
+// const asyncHandler = require('express-async-handler');
 
-const { Business } = require('../../db/models');
+// const { Business } = require('../../db/models');
 
-router.get( '/',
-    asyncHandler(
-        async (req, res) => {
-          const businesses = await Business.findAll();
-          return await res.json(businesses);
-    })
-);
+// router.get( '/',
+//     asyncHandler(
+//         async (req, res) => {
+//           const businesses = await Business.findAll();
+//           return await res.json(businesses);
+//     })
+// );
 
-router.post('/:id',
-    asyncHandler(
-        async (req, res) => {
-        businessId = req.params.id
-          const business = await Business.findByPk(businessId);
-          return await res.json(business);
-    })
-)
-module.exports = router;
+// router.post('/:id',
+//     asyncHandler(
+//         async (req, res) => {
+//         businessId = req.params.id
+//           const business = await Business.findByPk(businessId); where: businessId === id for reviews to each business
+//           return await res.json(business);
+//     })
+// )
+// module.exports = router;
