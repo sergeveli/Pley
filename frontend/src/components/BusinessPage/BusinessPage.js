17e042bab56efecb2cc4001434954bc4c3945530
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-07-23 16:41:34 -0400

    TryingHerokuAgain

--- frontend/src/components/BusinessPage/BusinessPage.js
+++ frontend/src/components/BusinessPage/BusinessPage.js
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react'
 import  { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
-import {getSingleBusiness} from '../../store/businessReducer'
+import {getSingleBusiness, } from '../../store/businessReducer'
 
 
 const BusinessPage = () =>{
