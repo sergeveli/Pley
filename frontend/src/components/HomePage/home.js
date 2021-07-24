599b75355e17fef9a7faaa735968c659229fc1a9
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-07-24 03:22:06 -0400

    HomeStyling1.0

--- frontend/src/components/HomePage/home.js
+++ frontend/src/components/HomePage/home.js
@@ -1,7 +1,10 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import {getAllBusinesses} from '../../store/businessReducer'
 import { useParams } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import './home.css'
+
+import {getAllBusinesses} from '../../store/businessReducer'
 
 const Home = () => {
     const dispatch = useDispatch();
@@ -17,21 +20,46 @@ const Home = () => {
     if(businesses){
         businessList = businesses.map(business=>(
         <div className='business_container'>
-            <div className='Name'>{business.title}</div>
-            <div className='Description'>{business.description}</div>
-            <div className='Address'>{business.address}</div>
+            <div className='avatar'> 
+            {/* figure out how to pull gymImg  */}
+                <img src={business.gymImg}/>
+            </div>
+            <h1 className='name'>
+                <NavLink to={`/${business.id}`}> 
+                {business.title}
+                </NavLink>
+            </h1>
+            <li className='description'>
+                {business.description}
+            </li>
+            <li className='location'>
+                {business.location}
+            </li>
+            <div className='details'> 
+            {/* figure out how to style x size this */}
+                <button>
+                    <p>DETAILS</p>
+                </button>
+            </div>
+            {/* <div className='Address'>{business.address}</div>
             <div className='City'>{business.city}</div>
             <div className='State'>{business.state}</div>
             <div className='Zip'>{business.zip}</div>
-            <div className='Location'>{business.location}</div>
+            <div className='Location'>{business.location}</div> */}
         </div>
         ))
     }
 
     return (
     <div>
+        <div className='new_Gym'> 
+            {/* figure out how to style x size this */}
+            <button>
+                <p>New Gym?</p>
+            </button>
+        </div>
         <div className='Tagline'>
-            <p>Let's Pley</p></div>
+            <p>Make A Pley</p></div>
         {/* <div className='search_bar'>
             <input className='searchbox' type='text'></input>
         </div> */}
--- frontend/src/store/businessReducer.js
+++ frontend/src/store/businessReducer.js
@@ -1,7 +1,14 @@
 import { ThunkAction } from "redux-thunk"
+import csrfFetch from "./csrf"
 
+//ACTION TYPES x CREATORS
 const LOAD_BUSINESS = 'businesses/LOAD_BUSINESS'
 const LOAD_BUSINESSES = 'businesses/LOAD_BUSINESSES'
+const NEW_BUSINESS = 'business/ADD_BUSINESS'
+const EDIT_BUSINESS = 'business/EDIT_BUSINESS'
+const DELETE_BUSINESS = 'businesses/DELETE_BUSINESS'
+const ADD_REVIEW = 'review/ADD_REVIEW'
+//const EDIT_REVIEW = 
 
 const loadBusiness = (business) => ({
     type: LOAD_BUSINESS,
@@ -12,6 +19,57 @@ const loadBusinesses = (businesses) =>({
     type: LOAD_BUSINESSES,
     payload: businesses
 })
+
+const newBusiness = (business) =>({
+    type: NEW_BUSINESS,
+    payload: business
+})
+const deleteBusiness = (business) =>({
+    type: DELETE_BUSINESS,
+    payload: business
+})
+
+const editBusiness = (business) => ({
+    type: EDIT_BUSINESS,
+    payload: business
+})
+
+const addReview = (review) => ({
+    type: ADD_REVIEW,
+    payload: review
+})
+
+//THUNK CREATORS
+//(C)
+export const addBusiness = (
+    ownerId,
+    title,
+    description,
+    address,
+    city,
+    state,
+    zip,
+    gymImg) => async(dispatch) => {
+        const response = await fetch(`api/business/new`,{
+            method: 'POST',
+            headers:{'Content-Type': 'application/json'},
+            body: JSON.STRINGIFY({
+                ownerId,
+                title,
+                description,
+                address,
+                city,
+                state,
+                zip,
+                gymImg            
+        })})
+        if(response.ok){
+            const business = await response.json()
+            console.log(business)
+            dispatch(newBusiness(business))
+        }
+}
+//(R)
 export const getAllBusinesses = () => async(dispatch) =>{
     const response = await fetch('/api/business/')
     if(response.ok){
@@ -19,7 +77,7 @@ export const getAllBusinesses = () => async(dispatch) =>{
         console.log(businesses)
         dispatch(loadBusinesses(businesses))
     }
-    }
+}
 
 
 export const getSingleBusiness = (businessId) => async(dispatch) => {
@@ -30,10 +88,76 @@ export const getSingleBusiness = (businessId) => async(dispatch) => {
         dispatch(loadBusiness(business))   
     }
 }
+
+//(U)
+export const editSingleBusiness = (businessId) => async(dispatch) =>{
+    const response = await fetch(`/api/business/${businessId}`,{
+        method: 'PUT',
+        headers:{'Content-Type' : 'application/json'},
+        body: JSON.stringify(businessId)
+    })
+    if(response.ok){
+    const business = await response.json()
+    console.log(business)
+    dispatch(editBusiness(business))
+    }
+}
+
+
+//(D)
+export const deleteSingleBusiness = (businessId) => async(dispatch) => {
+    const response = await fetch(`/api/business/${businessId}`, {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(business)
+    })
+    if(response.ok){}
+    const business = await response.json()
+    console.log('All Gone')
+    dispatch(deleteBusiness(business))
+
+}
+
+export const addSingleReview = (review) => async(dispatch) =>{
+    console.log(review)
+    const response = await csrfFetch(`/api/business/${review.businessId}/${review.userId}/review/new`, {
+            method: 'POST',
+            headers:{'Content-Type': 'application/json'},
+            body: JSON.STRINGIFY(review)            
+        })
+        console.log(review)
+        if(response.ok){
+            const newReview = await response.json()
+            dispatch(addReview(newReview))
+            return newReview
+        }
+}
+
+
+
+
+
+
+
+// when im adding a review..make a copy of the state then, find the business being updated(businessId) 
+// const reviews = newState[businessId].Reviews 
+// reviews.push(action.payload)
+
+
+
+//INITIAL STATE
 const initialState = {allBusiness:null, singleBusiness:null}
 
+
+//REDUCERS
 const businessReducer = (state = initialState, action)=>{
     switch(action.type){
+        case NEW_BUSINESS:{
+            return {
+                ...state,
+                newBusiness: action.payload
+            }
+        }
         case LOAD_BUSINESS:{
             return {
             ...state,
@@ -44,6 +168,18 @@ const businessReducer = (state = initialState, action)=>{
             return {
                 ...state,
                 allBusiness: action.payload
+            }
+        }
+        case EDIT_BUSINESS:{
+            return {
+                ...state,
+                singleBusiness: action.payload
+            }
+        }
+        case DELETE_BUSINESS:{
+            return {
+                ...state,
+                singleBusiness: action.payload
             }
         }
         default: 
