54e41970c7f94358b258ce267ea397e29d70661b
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-11-02 12:32:25 -0400

    SplashPage added and styled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Switch, Redirect } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 import BusinessPage from './components/BusinessPage/BusinessPage.js'
+import SplashPage from "./components/SplashPage";
 import Home from './components/HomePage/home'
 
 function App() {
+  const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
@@ -19,18 +21,47 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
+          <PrivateRoute exact path='/home'>
+            <Home user={user}/>
+          </PrivateRoute>
+
           <Route path="/signup" exact={true}>
             <SignupFormPage />
           </Route>
           <Route path='/:businessId/'>
             <BusinessPage />
           </Route> 
+
+          <Route path='/'>
+            {user ?
+            <Redirect to={{pathname: "/home"}}/> :
+            <SplashPage />
+            }
+          </Route>
         </Switch>
       )}
     </>
+  );
+}
+
+function PrivateRoute({ children, ...rest }) {
+  const user = useSelector(state => state.session.user);
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        user ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
   );
 }
 
